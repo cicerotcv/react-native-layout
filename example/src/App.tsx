40453ad820f8995731ec8d30ccea7e1e7a8fd468
cicerotcv <elm.tiago@gmail.com>
2024-04-19 19:50:48 -0300

    feat: implements skeleton component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,38 @@
 import * as React from 'react';
 
-import { Layout, Typography } from '@cicerotcv/react-native-layout';
+import { Layout, Skeleton, Typography } from '@cicerotcv/react-native-layout';
+import { Easing } from 'react-native-reanimated';
+import type { ViewStyle } from 'react-native';
+
+const CustomSkeleton = (props: ViewStyle) => (
+  <Skeleton
+    animationConfig={{
+      minOpacity: 0.4,
+      maxOpacity: 0.8,
+      stepDuration: 400,
+      easing: Easing.inOut(Easing.ease),
+    }}
+    {...props}
+  />
+);
 
 export default function App() {
   return (
     <Layout
       flex={1}
-      backgroundColor="gray"
       justifyContent="center"
-      alignItems="center"
+      alignItems="flex-start"
+      paddingHorizontal={16}
+      gap={16}
       viewProps={{
         collapsable: true,
       }}
     >
+      <Layout gap={8}>
+        <CustomSkeleton width={140} />
+        <CustomSkeleton width={150} />
+        <CustomSkeleton width={140} />
+      </Layout>
       <Typography
         color="red"
         backgroundColor="black"
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export { Layout } from './components/Layout';
 export { Typography } from './components/Typography';
+export { Skeleton } from './components/Skeleton';
